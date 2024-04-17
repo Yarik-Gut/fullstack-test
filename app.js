01ddfbe39e4656d0112366194397bcae5878a6e2
Yarik_Gut <FeiLongWu88>
2024-04-17 16:59:12 +0300

    Add cache 5 min

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const newsRouter = require('./routes/news');
-const keys = require('./config/keys')
+const keys = require('./config/keys');
+const NodeCache = require('node-cache');
+const cache = new NodeCache({ stdTTL: 300 });
+
 
 app.use(express.json());
 
@@ -14,5 +17,20 @@ app.use(require('cors')())
 mongoose.connect(keys.MONGODB_URI)
     .then(() => console.log('Підключено до MongoDB...'))
     .catch(err => console.error('Не вдалося підключитися до MongoDB...', err));
+
+app.use((req, res, next) => {
+    const key = req.originalUrl || req.url;
+    const cachedData = cache.get(key);
+    if (cachedData) {
+        res.json(cachedData);
+    } else {
+        res.sendResponse = res.json;
+        res.json = (body) => {
+            cache.set(key, body);
+            res.sendResponse(body);
+        };
+        next();
+    }
+});
 
 module.exports = app;
